ccc7eae54c15f35ba03acb416c1a298bf1983aa2
DK0423-duck <dongkyu2449@gmail.com>
2026-01-27 16:55:58 +0900

    init: spring boot project

---
+++ src/main/java/com/dramatalk/DramatalkApplication.java
@@ -0,0 +1,13 @@
+package com.dramatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramatalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramatalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dramatalk/DramatalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dramatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramatalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
